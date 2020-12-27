65ee76057e9320fd4b74749111258e68c9759ac4
Marianna Belniok <mariannabelniok@outlook.com>
2020-12-27 20:00:22 -0300

    finished

--- script.js
+++ script.js
@@ -6,3 +6,50 @@ modalClose.addEventListener("click", () => {
 modalClose.addEventListener("click", () => {
     modal.classList.add("hide")
 })
+
+// GLIDE CONFIG
+
+const bannerConfig = {
+    type: 'carousel',
+}
+const cardsConfig = {
+    breakpoints: {
+        1920: {
+            peek: 60,
+            perView: 5,
+        },
+        800: {
+            peek: 30,
+            perView: 3,
+        },
+        480: {
+            gap: 15,
+            peek: 30,
+            perView: 1,
+        }
+    }
+}
+const brendsConfig = {
+    breakpoints: {
+        1920: {
+            startAt:0,
+            peek: 60,
+            perView: 5,
+        },
+        800: {
+            peek: 30,
+            perView: 5,
+        },
+        480: {
+            startAt: 2,
+            gap: 15,
+            peek: 30,
+            perView: 1,
+        }
+    }
+}
+
+new Glide('.banner-carousel', bannerConfig).mount()
+new Glide('.ads-cards-carousel', cardsConfig).mount()
+new Glide('.brends-cards-carousel', brendsConfig).mount()
+new Glide('.prod-cards-carousel', cardsConfig).mount()
